0c7fb870a62f2f298e7057bccad26de40dff83e5
Thiện Lâm <lamminhthien02012000@gmail.com>
2024-07-01 14:39:03 +0700

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,7 @@ import { Cron } from '@nestjs/schedule';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!2';
   }
 
   @Cron('45 * * * * *')
